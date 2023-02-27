3761b1c4d4d1328d6bf9ae4b8131104c1dfe8b9c
maheshpulusu <122970744+maheshpulusu@users.noreply.github.com>
2023-02-27 14:37:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIA2XJ25D2GGJHELC6Z"
+  default = "AKIA2XJ25D2GCTTPNGPY"
   }
 variable "secret_key" {
-  default = "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"
+  default = "iyowLsYFwjFG7EVeqZ2j6Y9r6MDOImZzhQkeBUKn"
   }
 variable "region" {
   default = "ap-south-1"
